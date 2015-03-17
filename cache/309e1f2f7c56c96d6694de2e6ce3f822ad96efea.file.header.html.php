b79e344bd490b73256412ff48c9b709e9ec8da82
pukiscuy <pukiscuy@gmail.com>
2015-03-18 06:41:38 +0700

    register

--- cache/309e1f2f7c56c96d6694de2e6ce3f822ad96efea.file.header.html.php
+++ cache/309e1f2f7c56c96d6694de2e6ce3f822ad96efea.file.header.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2015-03-17 13:23:03
+<?php /* Smarty version Smarty-3.1.15, created on 2015-03-18 07:35:01
          compiled from "app/view/template/header.html" */ ?>
 <?php /*%%SmartyHeaderCode:119193434355066c36a96a91-35689625%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '309e1f2f7c56c96d6694de2e6ce3f822ad96efea' => 
     array (
       0 => 'app/view/template/header.html',
-      1 => 1426569779,
+      1 => 1426635297,
       2 => 'file',
     ),
   ),
@@ -17,12 +17,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'version' => 'Smarty-3.1.15',
   'unifunc' => 'content_55066c36b1a6e3_20316440',
+  'variables' => 
+  array (
+    'basedomain' => 0,
+  ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
 <?php if ($_valid && !is_callable('content_55066c36b1a6e3_20316440')) {function content_55066c36b1a6e3_20316440($_smarty_tpl) {?><!-- Fixed navbar -->
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade bs-example-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header" style="background-color:#26211C;color:#FFF">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -31,13 +35,18 @@ $_valid = $_smarty_tpl->decodeProperties(array (
       <div class="modal-body">
        <form>
           <div class="form-group">
-            <label for="recipient-name" class="control-label">Username</label>
+            <label for="recipient-name" class="control-label">Email</label>
             <input type="text" class="form-control" id="recipient-name" placeholder="type your email">
           </div>
           <div class="form-group">
-            <label for="message-text" class="control-label">PassWord</label>
+            <label for="message-text" class="control-label">Password</label>
             <input type="password" class="form-control" id="recipient-name" placeholder="type your password">
           </div>
+          <ol class="breadcrumb">
+            <li><a href="#">Forgot Password</a></li>
+            <li><a href="<?php echo $_smarty_tpl->tpl_vars['basedomain']->value;?>
+home/register">Sign Up(Daftar)</a></li>
+          </ol>
         </form>
       </div>
       <div class="modal-footer" style="background-color:#26211C;">
